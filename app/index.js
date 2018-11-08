299608cd3de2e3d30f2f10fa61b00b5a8052197f
lmy98129 <41624140@xs.ustb.edu.cn>
2018-11-08 22:35:31 +0800

    node server for testing added

--- app/index.js
+++ app/index.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import ReactDOM from 'react-dom'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import { Provider, connect } from 'react-redux'
+import thunk from 'redux-thunk'
 
 // React component
 class Table extends Component {
   render() {
-    const { value, onGetButtonClick } = this.props;
+    const { value, onGetButtonClickAsync } = this.props;
     return (
       <div>
         <span>
           {value}
         </span>
-        <button onClick={onGetButtonClick}>GetValue</button>
+        <button onClick={onGetButtonClickAsync}>GetValue</button>
       </div>
     )
   }
@@ -24,21 +25,41 @@ Table.propTypes = {
 }
 
 // Action
-const getValue = {type: 'getValue'}
+const actionTypes = {
+  GET_VALUE: 'GET_VALUE',
+  GET_VALUE_LOADING: 'GET_VALUE_LOADING',
+  GET_VALUE_SUCCESS: 'GET_VALUE_SUCCESS',
+  GET_VALUE_FAILED: 'GET_VALUE_FAILED',
+}
 
 // Reducer
 function valueReducer(state = { value: "" }, action) {
   const value = state.value
   switch (action.type) {
-    case 'getValue':
+    case 'GET_VALUE':
       return { value: value + "Good " }
+    case 'GET_VALUE_LOADING':
+      return {
+        value: "Loading"
+      }
+    case 'GET_VALUE_SUCCESS':
+      return {
+        value: ""
+      }
+    case 'GET_VALUE_FAILED':
+      return {
+        value: "Failed"
+      }
     default:
       return state
   }
 }
 
 // Store
-const store = createStore(valueReducer);
+const store = createStore(
+  valueReducer,
+  applyMiddleware(thunk)
+);
 
 // Map Redux state to component props
 function mapStateToProps(state) {
@@ -47,10 +68,49 @@ function mapStateToProps(state) {
   }
 }
 
+const getValueAsync = (url, init) => {
+  return (dispatch, getState) => {
+    fetch(url, init)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+      dispatch({
+        type: actionTypes.GET_VALUE_SUCCESS,
+      })
+    })
+    .catch(err => {
+      console.log(err);
+      dispatch({type: actionTypes.GET_VALUE_FAILED})
+    })
+  }
+}
+
 // Map Redux actions to component props
 function mapDispatchToProps(dispatch) {
   return {
-    onGetButtonClick: () => dispatch(getValue)
+    onGetButtonClick: () => dispatch(actionTypes.GET_VALUE),
+    onGetButtonClickAsync: () => {
+      dispatch(getValueAsync(
+      //   'http://elearning.ustb.edu.cn/choose_courses/j_spring_security_check', {
+      //   method: 'POST',
+      //   headers: {
+      //     "Content-Type": "application/x-www-form-urlencoded",
+      //   },
+      //   body: 'j_username=41624140,undergraduate&j_password=lmylmylmy98129',
+      //   // credentials: 'include',
+      //   mode: "no-cors"
+      // }
+      'http://student.bkthink.com/smvc/StuLoginService/loginStudentByIdentity.json', {
+          method: 'POST',
+          headers: {
+            "Content-Type": "application/json;charset=UTF-8",
+          },
+          body: "idNo=350721199801291815&loginApi=/StuLoginService/loginStudentByIdentity.json&secrite=291815&stuNo=41624140",
+          credentials: 'include',
+          mode: "no-cors"
+        }
+      ));
+    }
   }
 }
 
---
+++ server/server.js
@@ -0,0 +1,42 @@
+const http = require("http");
+const urlib = require("url");
+const querystring = require("querystring");
+let request = require("request-promise");
+request = request.defaults({jar: true});
+
+http.createServer((req, res) => {
+  let args = urlib.parse(req.url).query;
+  let params = querystring.parse(args);
+  if (req.url == '/login') {
+    request({
+      url: 'http://student.bkthink.com/smvc/StuLoginService/loginStudentByIdentity.json', 
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json;charset=UTF-8",
+      },
+      body: JSON.stringify({
+        "idNo": "350721199801291815",
+        "loginApi": "/StuLoginService/loginStudentByIdentity.json",
+        "secrite": "291815",
+        "stuNo": "41624140"
+      }),
+    })
+    .then(resp => {
+      return request({
+        url: "http://student.bkthink.com/smvc/StuQueryInfoService/viewStuCourseSchedule.json",
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json;charset=UTF-8",
+        }
+      })
+    })
+    .then (resp => {
+      console.log(resp);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+  }
+}).listen(1221, () => {
+  console.log('server is listening on port 1221');
+})
